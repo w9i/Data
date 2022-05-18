f805a47203feb631a9c836b2e12caa1969197d99
w9i <50361448+w9i@users.noreply.github.com>
2022-05-18 16:35:53 +0800

    Add files via upload

--- Package.swift
+++ Package.swift
@@ -5,22 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "Data",
+    platforms: [
+        .iOS(.v10)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Data",
             targets: ["Data"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(path: "../Common"),
+        .package(path: "../Domain"),
+        .package(path: "../Networking")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Data",
-            dependencies: []),
+            dependencies: ["Common", "Domain", "Networking"]),
         .testTarget(
             name: "DataTests",
             dependencies: ["Data"]),
--- Sources/Data/Network/APIEndpoints.swift
+++ Sources/Data/Network/APIEndpoints.swift
@@ -1,28 +1,28 @@
 //
-//  APIEndpoints.swift
+//  MovieEndpoints.swift
 //  ExampleMVVM
 //
 //  Created by Oleh Kudinov on 01.10.18.
 //
 
 import Foundation
+import Domain
+import Networking
 
 struct APIEndpoints {
     
-    static func getMovies(with moviesRequestDTO: MoviesRequestDTO) -> Endpoint<MoviesResponseDTO> {
-
-        return Endpoint(path: "3/search/movie/",
-                        method: .get,
-                        queryParametersEncodable: moviesRequestDTO)
+    static func movies(query: String, page: Int) -> DataEndpoint<MoviesPageResponse> {
+        
+        return DataEndpoint(path: "3/search/movie/",
+                            queryParameters: ["query": query,
+                                              "page": "\(page)"])
     }
-
-    static func getMoviePoster(path: String, width: Int) -> Endpoint<Data> {
-
-        let sizes = [92, 154, 185, 342, 500, 780]
+    
+    static func moviePoster(path: String, width: Int) -> DataEndpoint<Data> {
+        
+        let sizes = [92, 185, 500, 780]
         let closestWidth = sizes.enumerated().min { abs($0.1 - width) < abs($1.1 - width) }?.element ?? sizes.first!
         
-        return Endpoint(path: "t/p/w\(closestWidth)\(path)",
-                        method: .get,
-                        responseDecoder: RawDataResponseDecoder())
+        return DataEndpoint(path: "t/p/w\(closestWidth)\(path)")
     }
 }
